9f5f10e740d259da8bbd8fcb6e08f1e900e7a737
AlecrimCloudwalk <alecrim@cloudwalk.io>
2025-02-12 16:04:48 -0300

    Update for Node.js v10 compatibility

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-import express from 'express';
-import { promises as fs } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 
